d20506337eba4aaa7c65ccb306df5081ef452d46
Pancee <panseeh.wahid.99@gmail.com>
2022-01-02 11:52:32 +0200

    Update NoteController.php

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -10,5 +10,43 @@ use Illuminate\Http\Request;
 use \Illuminate\Http\JsonResponse;
 class NoteController
 {
+    public function addNote(Request $request){ //note_id
 
+    }
+
+    public function deleteNote(Request $request){ //note_id
+
+    }
+
+    public function setTitle(Request $request){ //note_id, title
+
+    }
+
+    public function setContent(Request $request){ //note_id, content
+
+    }
+
+    public function setCategory(Request $request){ //note_id, category
+
+    }
+
+    public function pinNote(){ //note_id
+
+    }
+
+    public function shareNote(Request $request){ //note_id, collaborator, asCopy
+
+    }
+
+    public function sortNotesByTitle(Request $request){
+
+    }
+
+    public function createNoteCategory(Request $request){ //title
+
+    }
+
+    public function editNote(Request $request){ //details
+
+    }
 }
