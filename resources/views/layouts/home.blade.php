8502811bd056b1916c4c8254051f534bccc359e6
Sam <58575241+ssammlek@users.noreply.github.com>
2022-01-04 22:00:55 +0200

    Note editing

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -149,58 +149,94 @@
             </div>
             <div id="shareNotes" style="display: none" class="container-fluid px-4">
                 <br>
-                <form id="sharingNotes">
-                <label for="userShared">Collaborator</label>
-                <input name="username" placeholder="Username" required>
-                <button type="submit">Share</button>
-                <br><br>
+                <form id="sharingNotes" method="POST">
+                    <label for="userShared">Collaborator</label>
+                    <input type="text" style="display: none" id="shared_note">
+                    <input name="username" placeholder="Username" required>
+                    <button id="share" type="submit">Share</button>
+                    <br><br>
+                    <div id="notesToBeShared">
+                    </div>
                 </form>
-                <button onclick="shareNotesView()">fuck</button>
+            </div>
+            <div id="addNotes" style="display: none" class="container-fluid px-4">
+                <br>
+                {{--                <div style="cursor: pointer; width: 70px" onclick="cancel()">--}}
+                {{--                    <p><u>< Cancel</u></p>--}}
+                {{--                </div>--}}
+                <div class="card card-margin">
+                    <form action="{{route('addNote')}}" method="POST">
+                        @csrf
+                        <input name="title" class="noteContent" id="addedNoteTitle" style="height: 50px;" placeholder="Note Title" maxlength="100" required>
+                        <textarea name="note_content" class="noteContent" id="addedNoteContent" style="height: 400px; border-bottom: none" placeholder="Note Content" required></textarea>
+                        <div>
+                            <label style="margin-left: 20px" for="cat">Category</label>
+                            <select name="category" style="width: 150px" name="cat" id="cat">
+                                <option selected value>Uncategorized</option>
+                            </select>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-sm btn-flash-border-success" style="float: right; ">ADD NOTE</button>
+                        </div>
+                    </form>
+
+                </div>
             </div>
             <div id="editNotes" style="display: none" class="container-fluid px-4">
-                <div style="cursor: pointer; width: 70px" onclick="cancel()">
-                    <p><u>< Cancel</u></p>
-                </div>
+                <br>
+                {{--                <div style="cursor: pointer; width: 70px" onclick="cancel()">--}}
+                {{--                    <p><u>< Cancel</u></p>--}}
+                {{--                </div>--}}
                 <div class="card card-margin">
-                    <form>
-                    <input class="noteContent" id="noteTitle" style="height: 50px;" placeholder="Note Title" maxlength="100" required>
-                    <textarea class="noteContent" id="noteContent" style="height: 400px; border-bottom: none" placeholder="Note Content" required></textarea>
-                    <div>
-                        <label style="margin-left: 20px" for="cat">Category</label>
-                        <select style="width: 150px" name="cat" id="cat">
-                            <option value="Defualt">Default</option>
-                        </select>
-                    </div>
-                    <div>
-                        <button type="submit" onsubmit="" class="btn btn-sm btn-flash-border-success" style="float: right; ">SAVE NOTE</button>
-                    </div>
+                    <form action="{{route('editNote')}}" method="POST">
+                        @csrf
+                        <input name="title" class="noteContent" id="noteTitle" style="height: 50px;" placeholder="Note Title" maxlength="100" required>
+                        <input id="note_id" type="text" name="note_id" style="display: none">
+                        <textarea name="note_content" class="noteContent" id="noteContent" style="height: 400px; border-bottom: none" placeholder="Note Content" required></textarea>
+                        <div>
+                            <label style="margin-left: 20px" for="cat">Category</label>
+                            <select name="category" style="width: 150px" name="cat" id="cat">
+                                <option disabled>Uncategorized</option>
+                            </select>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-sm btn-flash-border-success" style="float: right; ">EDIT NOTE</button>
+                        </div>
                     </form>
+
                 </div>
             </div>
             <div id="allNotes" style="display: none" class="container-fluid px-4">
                 <h1 class="mt-4" style="width: 400px;display: inline-block">Notes</h1>
-{{--                <input type="button" value="Create Category" style="display: inline-block">--}}
+                {{--                <input type="button" value="Create Category" style="display: inline-block">--}}
                 <ol class="breadcrumb mb-4">
-                    <li class="breadcrumb-item active">All</li>
+                    <li id="categoryNamePreview" class="breadcrumb-item active">All</li>
                 </ol>
+
                 <div  style="float: right;margin-right: 100px">
                     <form>
-                    <label for="categoryCreation">Create Category:</label>
-                    <input type="text" name="category" id="categoryCreation" placeholder="Category Title" required>
-                    <input type="submit" value="Create">
+                        <label for="categoryCreation">Create Category:</label>
+                        <input type="text" name="category" id="categoryCreation" placeholder="Category Title" required>
+                        <input onclick="categoryView('duck')" type="submit" value="Create">
                     </form>
                 </div>
                 <br>
-                <div style="cursor: pointer ;margin-right: 100px" onclick="show('editNotes')" >
+                <div style="cursor: pointer ;width:120px;  margin-right: 100px" onclick="show('addNotes')" >
                     <svg style="display: inline-block" class="svg-inline--fa fa-sticky-note fa-w-14" aria-hidden="true" focusable="false" data-prefix="far" data-icon="sticky-note" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M448 348.106V80c0-26.51-21.49-48-48-48H48C21.49 32 0 53.49 0 80v351.988c0 26.51 21.49 48 48 48h268.118a48 48 0 0 0 33.941-14.059l83.882-83.882A48 48 0 0 0 448 348.106zm-128 80v-76.118h76.118L320 428.106zM400 80v223.988H296c-13.255 0-24 10.745-24 24v104H48V80h352z"></path></svg>
                     <p style="display: inline-block" class="breadcrumb-item active" > Add new note</p>
                 </div>
-                <div class="container">
+                <div id="sortIcon" style="cursor: pointer;width: 20px" onclick="" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-down" viewBox="0 0 16 16">
+                        <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
+                    </svg>
+                </div>
+                <br>
+                <div class="container" >
                     <div class="row" id="notesContainer">
                     </div>
                 </div>
                 <div>
-                    <input type="button" value="Share Notes" style="background: inherit;border: none; float: right;" onclick="show('shareNotes')" >
+                    <input type="button" class="shareNotesB" value="Share Notes" style="background: inherit;border: none; float: right;" onclick="show('shareNotes')" >
                 </div>
             </div>
             <div id="sharedWithMeNotes" style="display: none" class="container-fluid px-4">
@@ -281,19 +317,35 @@
             show('allNotes');
         }
     }
-    function categoryView(){
+    function categoryView(category){
         var categoryContainer = document.getElementById("cat");
         var option = document.createElement("option");
-        option.innerText="Sad";
+        option.innerText=category;
         categoryContainer.insertBefore(option,null);
+        var aCategory = document.createElement("a");
+        aCategory.id=category;
+        aCategory.innerText=category;
+        aCategory.className="nav-link";
+        aCategory.onclick=function (){
+            document.getElementById("categoryNamePreview").innerText=category;
+            show('allNotes');
+            //
+        }
+        var categories =document.getElementById("notesCategories");
+        categories.insertBefore(aCategory,null);
+
     }
-    function shareNotesView(){
-        var form = document.getElementById("sharingNotes");
+    function shareNotesView(note){
+        var form = document.getElementById("notesToBeShared");
         var article = document.createElement("article");
         var noteShared= document.createElement("input");
         var div = document.createElement("div");
         noteShared.required=true;
-        div.innerText="Note";
+        article.onclick=function (){
+            document.getElementById("shared_note").id=note.note_id;
+        }
+        div.innerText=note.title;
+        noteShared.id=note.note_id;
         noteShared.type="radio";
         noteShared.name="category";
         // noteShared.required;
@@ -301,11 +353,13 @@
         article.insertBefore(noteShared,null);
         article.insertBefore(div,null);
         form.insertBefore(article,null);
+
     }
-    function noteView(){
+    function noteView(note){
         var notesContainer = document.getElementById("notesContainer");
         var col = document.createElement("div");
         col.className="col-lg-4";
+        col.id = note.note_id + "";
         var card = document.createElement("div");
         card.className="card card-margin";
         var cardHeader = document.createElement("div");
@@ -318,39 +372,82 @@
         view.className="widget-49-meeting-action";
         var cardTitleHeader = document.createElement("h5");
         cardTitleHeader.className="card-title";
-        cardTitleHeader.innerHTML="MOM";
-        cardHeader.style="width:50%;";
+        cardTitleHeader.innerHTML=note.title;
+        cardTitleHeader.style="float:left;width:95%;height:28px;text-overflow: ellipsis; overflow:hidden";
+        // cardHeader.style="width:100%;";
+
+        var pinned = note.pinned;
+
         var pinDiv= document.createElement("div");
         var pin= document.createElement("svg");
         pinDiv.style= "float:right;cursor:pointer";
-        pinDiv.onclick=function (){
-
-        }
-        pin.style= "float:right";
+        pin.style= "float:top";
         pin.xmlns="http://www.w3.org/2000/svg";
         pin.width='16';
         pin.height='16';
-        pin.className="bi bi-pin";
         pin.fill="currentColor";
         pin.viewBox="0 0 16 16";
+        if (pinned==true)
+            pin.className="bi bi-pin-fill";
+        else
+            pin.className="bi bi-pin";
+
         var path = document.createElement("path");
         path.d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354zm1.58 1.408-.002-.001.002.001zm-.002-.001.002.001A.5.5 0 0 1 6 2v5a.5.5 0 0 1-.276.447h-.002l-.012.007-.054.03a4.922 4.922 0 0 0-.827.58c-.318.278-.585.596-.725.936h7.792c-.14-.34-.407-.658-.725-.936a4.915 4.915 0 0 0-.881-.61l-.012-.006h-.002A.5.5 0 0 1 10 7V2a.5.5 0 0 1 .295-.458 1.775 1.775 0 0 0 .351-.271c.08-.08.155-.17.214-.271H5.14c.06.1.133.191.214.271a1.78 1.78 0 0 0 .37.282z";
         pin.insertBefore(path,null);
+        pinDiv.onclick=function (e){
+            if (pinned === 1){ //pin style
+                pinned = false;
+                pin.className="bi bi-pin";
+            }
+            else {
+                pinned = true;
+                pin.className="bi bi-pin-fill";
+            }
+            e.preventDefault();
+            var _token = $("input[name='_token']").val();
+            console.log(pinned);
+            $.ajax({
+                type: "POST",
+                url: "{{route('pinNote')}}",
+                data: {
+                    note_id: note.note_id,
+                    _token: _token,
+                },
+                success: function() {
+                    console.log("success");
+                }
+            });
+        }
+
         var noteContent = document.createElement("p");
+
         noteContent.className="widget-49-pro-title";
         noteContent.style="text-overflow: ellipsis; overflow-y:hidden;height: 290px;width:100%;";
-        noteContent.innerHTML="PRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera modulePRO-027865 Opera module"
+        noteContent.innerHTML= note.content;
+
         var viewNote = document.createElement("div");
         viewNote.innerHTML="VIEW NOTE";
         viewNote.className="btn btn-sm btn-flash-border-success";
         viewNote.onclick=function (){
+
+            document.getElementById("noteTitle").value= note.title;
+            document.getElementById("noteContent").innerText= note.content;
             show('editNotes');
+            document.getElementById("note_id").value = col.id;
         }
         const self= this;
         var deleteButton =document.createElement("svg");
         deleteButton.style= "float:left;cursor:pointer";
         deleteButton.onclick=function (){
-            notesContainer.removeChild(col);
+            var result = confirm("Are you sure you want to delete this note?");
+            if (result) {
+                document.getElementById("deletedNote").value = col.id;
+                document.forms["deleteNote"].action= "{{route('deleteNote')}}"
+                document.forms["deleteNote"].submit();
+                notesContainer.removeChild(col);
+            }
+
         }
         deleteButton.xmlns="http://www.w3.org/2000/svg";
         deleteButton.width='16';
