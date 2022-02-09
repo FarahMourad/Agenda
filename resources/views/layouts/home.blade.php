9330e16afb543e4b84b15327149eace90b4be051
Sam <58575241+ssammlek@users.noreply.github.com>
2022-02-09 15:30:50 +0200

    Update home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -308,8 +308,8 @@
                     <svg style="display: inline-block" class="svg-inline--fa fa-sticky-note fa-w-14" aria-hidden="true" focusable="false" data-prefix="far" data-icon="sticky-note" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M448 348.106V80c0-26.51-21.49-48-48-48H48C21.49 32 0 53.49 0 80v351.988c0 26.51 21.49 48 48 48h268.118a48 48 0 0 0 33.941-14.059l83.882-83.882A48 48 0 0 0 448 348.106zm-128 80v-76.118h76.118L320 428.106zM400 80v223.988H296c-13.255 0-24 10.745-24 24v104H48V80h352z"></path></svg>
                     <p style="display: inline-block" class="breadcrumb-item active" > ADD NEW NOTE</p>
                 </div>
-                <div id="sortIcon" style="cursor: pointer;width: 20px" onclick="" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-down" viewBox="0 0 16 16">
+                <div>
+                    <svg id="sortIcon" style="cursor: pointer;width: 20px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-down" viewBox="0 0 16 16">
                         <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
                     </svg>
                 </div>
@@ -476,12 +476,19 @@
         }
     }
     function categoryView(category){
-        var cat1 = document.getElementById("categ");
-        var cat2 = document.getElementById("editCat");
         var option = document.createElement("option");
         option.innerText=category;
+
+        var cat1 = document.getElementById("editCat");
         cat1.insertBefore(option,null);
-        cat2.insertBefore(option,null);
+
+        var option2 = document.createElement("option");
+        option2.innerText=category;
+        var cat2 = document.getElementById("categ");
+        cat2.insertBefore(option2,null);
+
+
+
         var aCategory = document.createElement("a");
         aCategory.id=category;
         aCategory.innerText=category;
